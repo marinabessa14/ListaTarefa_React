8c98e6cc930ff9617208cb58d7a80b41fd4f166a
marinabessa <marinabessarodrigues@hotmail.com>
2023-05-17 15:48:04 -0300

    Lista Tarefa

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+import Todo from './components/Todo';
+import {TodoForm}  from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
+
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false
+    },
+  ]);
+
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("");
+
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) => 
+    todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
+  
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+    setTodos(newTodos);
+
+  }
+
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} />
+    <div className="todo-list">
+      {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+        <Todo todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+  ))}
+  </div>
+  <TodoForm addTodo={addTodo} />
+  </div>
+
+}
+
+export default App
